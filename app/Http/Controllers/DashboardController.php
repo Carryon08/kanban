6276c6529850d866e28641488b4a745040a00524
Roberto Carreon <robertocmdga@hotmail.com>
2024-01-25 15:58:05 -0600

    create project

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\dashboard;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+/**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+}
